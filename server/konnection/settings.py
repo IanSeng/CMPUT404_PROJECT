8a4e2e574f57e4837e914bc7bcbedcbecbb92cf6
Ian Seng <ianseng@outlook.com>
2021-02-08 19:37:07 -0700

    add temporary string for secret key

--- server/konnection/settings.py
+++ server/konnection/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+# TODO: Repalce SECRET_KEY with actual key before production deployment
+SECRET_KEY = 'temporaryKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
