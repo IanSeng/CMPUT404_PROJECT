39d563b24cc8ffceb397ed491336268f4d02e59b
Ian Seng <ianseng@outlook.com>
2021-02-08 19:33:22 -0700

    remove secret key

--- server/konnection/settings.py
+++ server/konnection/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c3z_pc@e0cxlrh0@o3$%q3^y+)k_nqz=o@^+4^r**^-4^6m6n^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
