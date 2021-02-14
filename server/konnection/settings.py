e6f41029148b481cac382a451f7e72efc8cbd0a6
bui1@ualberta.ca <bui1@ualberta.ca>
2021-02-13 22:50:59 -0700

    #5 Create Author Model

--- server/konnection/settings.py
+++ server/konnection/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'konnection.authors',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
