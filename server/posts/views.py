1f879975b341c2336fa1fa244f9427f2ea228883
HailanXyouknow <hailan@ualberta.ca>
2021-02-15 21:58:45 -0700

    Post's GET and POST method is available

--- server/posts/views.py
+++ server/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
+from authors.models import Author
 
-# Create your views here.
+
+class PostListCreate(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    # TODO: use access token to figure out user in the future?
+    def perform_create(self, serializer):
+        serializer.save(author=Author.objects.get(user=self.request.user.id))
