bf6ee5a7614542fc7d43b4d729d6048acebeb4e8
HailanXyouknow <hailan@ualberta.ca>
2021-02-18 00:16:06 -0700

    uncomment test settings

--- server/konnection/settings.py
+++ server/konnection/settings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 
 # For tests
 # https://stackoverflow.com/a/35224204
-# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-# NOSE_ARGS = ['--with-spec', '--spec-color']
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = ['--with-spec', '--spec-color']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
