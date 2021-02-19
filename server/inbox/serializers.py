fce52752dae0de488102989df2d2c35c1727cb63
Kean Weng Yap <yapkeanweng1223@gmail.com>
2021-02-19 15:17:37 -0700

    Add hostname to Inbox author field.

--- server/inbox/serializers.py
+++ server/inbox/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
 from .models import Inbox
 from posts.serializers import PostSerializer
+from main import utils
 
 class InboxSerializer(serializers.ModelSerializer):
     items = serializers.SerializerMethodField()
+    author = serializers.SerializerMethodField()
 
     def get_items(self, obj):
         data = PostSerializer(obj.posts.all(), many=True).data
         return data
+
+    def get_author(self, obj):
+        return f"{utils.HOST}/author/{obj.author.id}"
 
     class Meta:
         model = Inbox
