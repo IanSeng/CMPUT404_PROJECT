6225a2deaf65ae6df89a52d8c57e077699c02985
bui1@ualberta.ca <bui1@ualberta.ca>
2021-03-02 15:35:49 -0700

    feat: post posts to my stream

--- client/src/components/Feeds/MyFeedPage.js
+++ client/src/components/Feeds/MyFeedPage.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { Card, Message, Dimmer, Loader } from "semantic-ui-react";
+import axios from "axios";
+import PostList from "../Post/PostList";
+import { SERVER_HOST } from "../../Constants";
+import { Context } from "../../Context";
 
 const MyFeedPage = () => {
-  return <div>My Feed</div>;
+  const context = useContext(Context);
+
+  const [posts, updatePosts] = useState([]);
+  const [error, updateError] = useState(false);
+  const [loading, updateLoading] = useState(true);
+
+  const getAllMyPosts = async () => {
+    try {
+      const response = await axios.get(
+        `${SERVER_HOST}/service/author/${context.user.id}/posts/`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Token ${context.cookie}`,
+          },
+        }
+      );
+      console.log(response);
+      updatePosts(response.data);
+    } catch (error) {
+      updateError(true);
+    }
+    updateLoading(false);
+  };
+
+  const getAllInboxItems = () => {};
+
+  useEffect(() => {
+    if (context.user) {
+      getAllMyPosts();
+      getAllInboxItems();
+    }
+  }, []);
+
+  return (
+    <div>
+      {loading && (
+        <Dimmer inverted active>
+          <Loader size="medium">Loading Posts...</Loader>
+        </Dimmer>
+      )}
+      {error && (
+        <Message
+          error
+          size="large"
+          header="Error"
+          content="Something happened on our end. Please try again later."
+        />
+      )}
+      <Card.Group centered itemsPerRow={1}>
+        <PostList posts={posts} />
+      </Card.Group>
+    </div>
+  );
 };
 
 export default MyFeedPage;
