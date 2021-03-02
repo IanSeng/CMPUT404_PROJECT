5bef4e086268a72e6d39448c84cb9807afbbc42f
bui1@ualberta.ca <bui1@ualberta.ca>
2021-03-01 23:40:02 -0700

    feat: add view other author profiles

--- client/src/components/Profile/ProfilePage.js
+++ client/src/components/Profile/ProfilePage.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
-import { Menu, Segment } from "semantic-ui-react";
+import React, { useState, useContext, useEffect } from "react";
+import { Menu, Segment, Message } from "semantic-ui-react";
+import { useLocation } from "react-router-dom";
 import "./ProfilePage.scss";
+import { Context } from "../../Context";
 import ProfileData from "./ProfileData";
 import FriendRequestList from "../Friends/FriendRequestList";
 import FriendList from "../Friends/FriendList";
 import FollowerList from "../Friends/FollowerList";
 import FollowingList from "../Friends/FollowingList";
+import { getUserObject } from "../../ApiUtils";
 
 const recentPosts = "Recent Posts";
 const friends = "Friends";
@@ -14,12 +17,35 @@ const following = "Following";
 const friendRequests = "Friend Requests";
 
 const MyProfilePage = () => {
-  const [activeItem, updateActiveItem] = useState(friendRequests);
-  const [currentSection, updateSection] = useState(<FriendRequestList />);
+  const context = useContext(Context);
+  const location = useLocation();
+
+  const [activeItem, updateActiveItem] = useState(friends);
+  const [currentSection, updateSection] = useState(<FriendList />);
+  const [error, updateError] = useState(false);
+  const [currentAuthor, updateCurrentAuthor] = useState({});
+
+  useEffect(() => {
+    const authorId = window.location.pathname.split("/").pop();
+    getOtherAuthorObject(authorId);
+  }, [location]);
 
   const handleItemClick = (e, { name, section }) => {
     updateActiveItem(name);
     updateSection(section);
+  };
+
+  const getOtherAuthorObject = async (authorId) => {
+    if (context.user) {
+      if (authorId === context.user.id) return;
+    }
+
+    try {
+      const response = await getUserObject(context.cookie, authorId);
+      updateCurrentAuthor(response.data);
+    } catch (error) {
+      updateError(true);
+    }
   };
 
   const placeholder = (
@@ -29,10 +55,24 @@ const MyProfilePage = () => {
     />
   );
 
+  const showElement = () => {
+    const authorId = window.location.pathname.split("/").pop();
+    return authorId === (context.user ? context.user.id : true);
+  };
+
   return (
     <div className="profile-page-container">
+      {error && (
+        <Message
+          error
+          size="large"
+          header="Error"
+          content="Something happened on our end. Please try again later."
+        />
+      )}
+
       <div className="profile-data">
-        <ProfileData />
+        <ProfileData author={currentAuthor} />
       </div>
 
       <div className="profile-posts">
@@ -61,12 +101,14 @@ const MyProfilePage = () => {
             onClick={handleItemClick}
             section={<FollowingList />}
           />
-          <Menu.Item
-            name={friendRequests}
-            active={activeItem === friendRequests}
-            onClick={handleItemClick}
-            section={<FriendRequestList />}
-          />
+          {showElement() && (
+            <Menu.Item
+              name={friendRequests}
+              active={activeItem === friendRequests}
+              onClick={handleItemClick}
+              section={<FriendRequestList />}
+            />
+          )}
         </Menu>
 
         <Segment attached="bottom">{currentSection}</Segment>
