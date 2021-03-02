40da8cb71af45ad8c8bd7216939b65f23973764b
Ian Seng <ianseng@outlook.com>
2021-03-02 11:00:21 -0700

    put edge cases done

--- server/followers/serializers.py
+++ server/followers/serializers.py
@@ -25,3 +25,5 @@ class FollowersModificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Followers
         fields = ('followers',)
+
+    
--- server/followers/views.py
+++ server/followers/views.py
@@ -1,9 +1,13 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from rest_framework import generics, permissions, status
+
+from django.contrib.auth import get_user_model
 
 from main import models
 from followers.serializers import FollowersSerializer, FollowersModificationSerializer
+from author.serializers import AuthorProfileSerializer
 
 
 class FollowersView(generics.RetrieveAPIView):
@@ -35,18 +39,18 @@ class FollowersModificationView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
  
     def get_object(self):
-        requestAuthorId = self.kwargs['id']
-        requestForeignAuthorId = self.kwargs['foreignId']
+        self.requestAuthorId = self.kwargs['id']
+        self.requestForeignAuthorId = self.kwargs['foreignId']
         
         if not self.request.user.adminApproval:
             raise AuthenticationFailed(
                 detail={"error": ["User has not been approved by admin"]})
 
         try:
-            self.author = models.Author.objects.get(id=requestAuthorId)
-            self.foreignAuthor = models.Author.objects.get(id=requestForeignAuthorId)
+            self.author = models.Author.objects.get(id=self.requestAuthorId)
+            self.foreignAuthor = models.Author.objects.get(id=self.requestForeignAuthorId)
                   
-            return models.Followers.objects.filter(author=requestAuthorId)
+            return models.Followers.objects.filter(author=self.requestAuthorId)
 
         except:
             raise ValidationError({"error": ["User not found"]})
@@ -54,8 +58,36 @@ class FollowersModificationView(generics.RetrieveUpdateDestroyAPIView):
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
+
         return Response({
             'status': self.foreignAuthor in serializer.data['followers'],
+            'author':  self.author.id,
+            'follower': self.foreignAuthor.id,
+        })
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.get_serializer(instance, data=request.data, partial=True)
+        
+        if (self.requestAuthorId == self.requestForeignAuthorId):
+            return Response({
+                'error': ['you cannot follow yourself']}, status=status.HTTP_400_BAD_REQUEST)
+        elif (not self.request.user.adminApproval):
+            raise AuthenticationFailed(
+                detail={"error": ["User has not been approved by admin"]})
+        elif (self.requestForeignAuthorId is not request.user.id):
+           return Response({
+                'error': ['this is not your account, you cannot follow this author']}, status=status.HTTP_403_FORBIDDEN) 
+        
+        authorObj = models.Author.objects.get(id=self.requestAuthorId)
+        author = models.Followers.objects.get(author=authorObj)
+        foreignAuthor = models.Author.objects.get(id=self.requestForeignAuthorId)
+
+        author.followers.add(foreignAuthor)
+        author.save()
+        
+        return Response({
+            'status': True,
             'author':  self.author.id,
             'follower': self.foreignAuthor.id,
         })
